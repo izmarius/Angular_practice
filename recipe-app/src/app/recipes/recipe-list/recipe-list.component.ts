dd1272f31ecbe9a7c8d27f261e5d62de8343b5aa
izmarius <izmarius.im@gmail.com>
2020-12-09 22:48:22 +0200

    update-recipe-list

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent  {
   recipes: Recipe[] = [
+    new Recipe('recipe-test', 'a recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
     new Recipe('recipe-test', 'a recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872')
   ];
 }
