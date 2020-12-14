878550f42406c036055be84b13114f8e638863cc
izmarius <izmarius.im@gmail.com>
2020-12-14 22:18:38 +0200

    navigation-with-ngif

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedPage = 'recipe';
 
+  // tslint:disable-next-line:typedef
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
 }
