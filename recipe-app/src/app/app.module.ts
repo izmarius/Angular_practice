a5debedf258f91a588771fd94e2abc98ea5690ee
izmarius <izmarius.im@gmail.com>
2020-12-21 22:46:09 +0200

    dropdown toggle directive

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DropdownDirective } from './shared/dropdown.directive';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -20,7 +21,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
    ],
   imports: [
     BrowserModule,
