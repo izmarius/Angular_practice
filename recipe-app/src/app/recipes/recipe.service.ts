10524aebae60dce460b08a13cd70f2a97992d4e1
izmarius <izmarius.im@gmail.com>
2021-01-01 20:03:56 +0200

    added ingredients to recipe model

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,23 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('recipe-test', 'a recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
-    new Recipe('recipe-test-2', 'a recipe 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872')
+    new Recipe(
+      'Chorizo Mozarella Gnocchi Baked',
+      'baked chorizzo with morarella and gnocchi',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+      [
+        new Ingredient('Chrizo', 2),
+        new Ingredient('Mozrella', 1),
+        new Ingredient('Potates', 2)
+      ]),
+    new Recipe(
+      'Shakshuka',
+      'tomato sos with poached eggs',
+      'https://media.eggs.ca/assets/RecipeThumbs/_resampled/FillWyIxMjgwIiwiNzIwIl0/Shakshuka-For-One.jpg',
+      [
+        new Ingredient('Eggs', 6),
+        new Ingredient('Tomatoes', 2)
+      ])
   ];
 
   // tslint:disable-next-line:typedef
